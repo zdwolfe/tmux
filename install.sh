26e6d18b716d741d170497fc01bf9db82470f1b6
Zach Wolfe <wolfe.zach@gmail.com>
2014-02-19 10:38:50 -0500

    More robust install.sh script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 DATE=`date +%s`
+
 if [ -f ~/.tmux.conf ]
 then
   cp -v ~/.tmux.conf ~/.tmux.$DATE.conf
 fi
 
 ln -sfv $DIR/tmux.conf ~/.tmux.conf
-echo "zsh config installed"
+echo "tmux config installed"
